224c587dcb2f520ad55227670a1714a78f023c69
nuptaxin <nuptaxin@gmail.com>
2017-04-11 14:26:36 +0800

    first commit

---
+++ src/main/java/ren/ashin/wechat/intfc/MyServer.java
@@ -0,0 +1,35 @@
+package ren.ashin.wechat.intfc;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ * @ClassName: MyServer
+ * @Description: Jetty程序启动入口
+ * @author renzx
+ * @date Apr 11, 2017
+ */
+public class MyServer {
+    public static void main(String[] args) throws Exception {
+        String warPath = "wechat-intfc.war";
+        String webapp = "src/main/webapp";
+
+        Server server = new Server(8080);
+
+        WebAppContext context = new WebAppContext();
+
+        // 使用war包打包前，请注释掉一下两行
+        context.setDescriptor(webapp + "/WEB-INF/web.xml");
+        context.setResourceBase(webapp);
+        // 使用Eclipse调试时，请把该行注释掉
+        // context.setWar(warPath);
+
+        context.setContextPath("/");
+        context.setClassLoader(Thread.currentThread().getContextClassLoader());
+
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+}
