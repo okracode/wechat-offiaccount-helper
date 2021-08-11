0df8ae745e552b7d5e73d12bdd9c313d47b6c307
zhengxin.ren <zhengxin.ren@amh-group.com>
2021-08-11 10:32:22 +0800

    init subscription-web module

---
+++ wx-subscription-web/src/main/java/com/okracode/wx/subscription/web/WxSubscriptionWebApplication.java
@@ -0,0 +1,13 @@
+package com.okracode.wx.subscription.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxSubscriptionWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxSubscriptionWebApplication.class, args);
+    }
+
+}
