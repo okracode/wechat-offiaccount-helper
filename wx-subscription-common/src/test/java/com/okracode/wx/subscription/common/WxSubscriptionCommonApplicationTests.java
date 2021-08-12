af001b23a95a58cc18ea849f25d50b6e9895830b
zhengxin.ren <zhengxin.ren@amh-group.com>
2021-08-12 17:07:41 +0800

    增加common/service 模块

---
+++ wx-subscription-common/src/test/java/com/okracode/wx/subscription/common/WxSubscriptionCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okracode.wx.subscription.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxSubscriptionCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ wx-subscription-service/src/test/java/com/okracode/wx/subscription/service/WxSubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okracode.wx.subscription.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxSubscriptionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
