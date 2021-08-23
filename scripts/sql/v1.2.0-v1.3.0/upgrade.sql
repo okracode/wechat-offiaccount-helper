b5725a6ada44bcd353c1719c06817b7bda325bcd
Eric Ren <nuptaxin@gmail.com>
2021-08-23 09:13:40 +0800

    修正upgrade脚本

--- scripts/sql/v1.2.0-v1.3.0/upgrade.sql
+++ scripts/sql/v1.2.0-v1.3.0/upgrade.sql
@@ -3,4 +3,4 @@ USE
 
 ALTER TABLE `wechat_msg`
     MODIFY COLUMN `createtime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '消息时间旧值';
-UPDATE `wechat_msg` SET `msg_time` = `createtime` WHERE `msg_time` != `createtime`;
+UPDATE `wechat_msg` SET `msg_time` = `createtime` WHERE `msg_time` - `createtime` != 0;
