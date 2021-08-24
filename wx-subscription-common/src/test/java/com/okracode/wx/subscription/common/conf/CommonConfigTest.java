70b52035d71e93e05b91eaff3eb03b20fad7623c
Eric Ren <nuptaxin@gmail.com>
2021-08-24 20:15:01 +0800

    完成common模块单元测试

---
+++ wx-subscription-common/src/test/java/com/okracode/wx/subscription/common/conf/CommonConfigTest.java
@@ -0,0 +1,36 @@
+package com.okracode.wx.subscription.common.conf;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import javax.annotation.Resource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.ResourceUtils;
+
+/**
+ * @author Eric Ren
+ * @date 2021/8/24
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class CommonConfigTest {
+
+    @Resource
+    private CommonConfig commonConfig;
+
+    @Test
+    public void testGetTulingRobot() throws IOException {
+        InputStream in = new BufferedInputStream(
+                new FileInputStream(ResourceUtils.getFile("classpath:application.properties")));
+        Properties p = new Properties();
+        p.load(in);
+        assertEquals(p.getProperty("tulingRobot"), commonConfig.getTulingRobot());
+    }
+}
