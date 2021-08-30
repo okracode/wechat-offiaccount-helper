b28783960ee300cdfc33dc6bc294c1f9fff31719
Eric Ren <nuptaxin@gmail.com>
2021-08-30 09:16:05 +0800

    增加service module的部分单元测试

--- wx-subscription-service/src/test/java/com/okracode/wx/subscription/service/WxSubscriptionServiceApplicationTests.java
+++ wx-subscription-service/src/test/java/com/okracode/wx/subscription/service/WxSubscriptionServiceApplicationTests.java
@@ -1,19 +1,42 @@
 package com.okracode.wx.subscription.service;
 
-import org.junit.jupiter.api.Test;
+import ch.vorburger.exec.ManagedProcessException;
+import ch.vorburger.mariadb4j.DB;
+import ch.vorburger.mariadb4j.DBConfigurationBuilder;
+import com.okracode.wx.subscription.service.util.ParseJsonTest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootTest
+@RunWith(Suite.class)
+@SuiteClasses(value = {ParseJsonTest.class})
 @SpringBootApplication
 @ComponentScan("com.okracode.wx.subscription")
 @MapperScan("com.okracode.wx.subscription.repository.dao")
 class WxSubscriptionServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    private static final int DB_PORT = 3309;
+    private static DB db;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        DBConfigurationBuilder builder = DBConfigurationBuilder.newBuilder();
+        builder.setPort(DB_PORT);
+        builder.addArg("--user=root");
+        builder.addArg("--enable-lower_case_table_names");
+        db = DB.newEmbeddedDB(builder.build());
+        db.start();
+        db.source("sql/init.sql");
+    }
+
+    @AfterClass
+    public static void afterClass() throws ManagedProcessException {
+        db.stop();
     }
 
 }
