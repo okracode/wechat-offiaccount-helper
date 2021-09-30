5d9f6db70d3cd1dac69cf910d012622a232397c4
Eric Ren <nuptaxin@gmail.com>
2021-09-30 11:14:27 +0800

    修改单元测试sql脚本与生产sql脚本一致

--- scripts/sql/init.sql
+++ scripts/sql/init.sql
@@ -2,8 +2,7 @@ SET
     FOREIGN_KEY_CHECKS = 0;
 CREATE
     DATABASE IF NOT EXISTS `wechat`;
-USE
-    `wechat`;
+USE `wechat`;
 -- ----------------------------
 -- Table structure for wechat_msg
 -- ----------------------------
@@ -21,6 +20,6 @@ CREATE TABLE `wechat_msg`
     `msgid`         varchar(255)          DEFAULT NULL,
     `create_time`   datetime(3)  NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '数据创建时间',
     `update_time`   datetime(3)  NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据修改时间',
-        PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
--- wechat-offiaccount-helper-repository/src/test/resources/sql/init.sql
+++ wechat-offiaccount-helper-repository/src/test/resources/sql/init.sql
@@ -9,7 +9,7 @@ USE `wechat`;
 DROP TABLE IF EXISTS `wechat_msg`;
 CREATE TABLE `wechat_msg`
 (
-    `id`            int(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id`            int(11)      NOT NULL AUTO_INCREMENT,
     `tousername`    varchar(255) NOT NULL,
     `fromusername`  varchar(255) NOT NULL,
     `msg_time`      datetime(3)  NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '消息时间',
@@ -19,6 +19,7 @@ CREATE TABLE `wechat_msg`
     `funcflag`      varchar(255)          DEFAULT NULL,
     `msgid`         varchar(255)          DEFAULT NULL,
     `create_time`   datetime(3)  NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '数据创建时间',
-    `update_time`   datetime(3)  NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据修改时间'
+    `update_time`   datetime(3)  NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据修改时间',
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
